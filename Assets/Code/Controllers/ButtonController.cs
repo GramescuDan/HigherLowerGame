8d8f2f597912e8d33fe424d54786acb7ad20531d
GramescuDan <gramescudan@yahoo.com>
2021-07-21 17:57:24 +0300

    Worked on controllers

--- Assets/Code/Controllers/ButtonController.cs
+++ Assets/Code/Controllers/ButtonController.cs
@@ -3,28 +3,33 @@ using Code.Models;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Random = System.Random;
+using UnityEngine.UI;
 
 namespace Code.Controllers
 {
-    public class ButtonFuncitonality : MonoBehaviour
+    public class ButtonController : MonoBehaviour
 
     {
         private static int nr;
         private int myNr;
         private Random random = new Random();
-        public TextField _myNumber;
-        public TextField _yourNumber;
+        public TextField _myNumber = new TextField();
+        public TextField _yourNumber = new TextField();
         public ButtonModel lower;
         public ButtonModel equal;
         public ButtonModel higher;
-        
-        
-        void Start()
+
+
+        private void Start()
         {
-            nr = random.Next(2,14);
+            nr = random.Next(2, 14);
             _myNumber.value += nr;
-            myNr = random.Next(2,14);
+            myNr = random.Next(2, 14);
         }
-        
+
+        private void Awake()
+        {
+            
+        }
     }
 }
